99bfa3cdc4f5f53dd70725dbfb5ae331d8398226
Zora <zora@ZoradeMacBook-Pro.local>
2025-02-21 19:17:08 -0500

    update firestore and frontend

---
+++ src/App.js
@@ -0,0 +1,17 @@
+// src/App.js
+import React from 'react';
+import Button from '@mui/material/Button';
+import Container from '@mui/material/Container';
+
+function App() {
+    return (
+    <Container>
+        <h1>欢迎使用 Firebase 后端和 MUI 前端</h1>
+        <Button variant="contained" color="primary">
+        点击我
+        </Button>
+    </Container>
+    );
+}
+
+export default App;
